e06f0b024aa439a0a5e097d7415b6292504317c1
GamedevBranislav <61313508+GamedevBranislav@users.noreply.github.com>
2023-04-22 18:27:22 +0100

    Add project files.

---
+++ TestingBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestingBank
+{
+    class Program
+    {
+        //Run this to get BankAccountHistory 
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account{account.Number} was created for {account.Owner} with {account.Balance}. ");
+
+
+            account.MakeWitdrawal(120, DateTime.Now, "Expensive Dinner");
+
+            account.MakeWitdrawal(335, DateTime.Now, "Ryanair Tickets");
+
+            account.MakeWitdrawal(60, DateTime.Now, "Hogwarts Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+            Console.WriteLine($"Actual balance is : {account.Balance} $");
+        }
+    }
+}
